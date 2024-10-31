3a8318155ff3af7880e0e59c5225f4d6403f8a16
akshayrajeevnambiar <akshayrajeevnambiar@gmail.com>
2024-10-31 17:35:37 -0400

    fixde migration issues

--- src/config.py
+++ src/config.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     
 class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     DEBUG = True
 
 config={
